c11a356d4a465cf5253888d3441bfd521da52850
Eric Internicola <intere@gmail.com>
2019-08-11 12:14:15 -0600

    Initial Commit

---
+++ 360iDev AR Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  360iDev AR Navigation
+//
+//  Created by Eric Internicola on 8/11/19.
+//  Copyright © 2019 Eric Internicola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
